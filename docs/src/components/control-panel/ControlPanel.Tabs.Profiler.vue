5ccd199141585cf82e1674877e60ecfbe3138750
raul <raulssorban@gmail.com>
2025-10-06 21:39:49 +0200

    mid-off

--- docs/src/components/control-panel/ControlPanel.Tabs.Profiler.vue
+++ docs/src/components/control-panel/ControlPanel.Tabs.Profiler.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { Download, Play, Square, Trash2 } from 'lucide-vue-next'
+import { Download, Play, Square, Trash2, X } from 'lucide-vue-next'
 import { deleteProfile, loadProfile, toggleProfile} from './ControlPanel.Profiler'
 import { selectedServer, save } from './ControlPanel.SaveLoad'
 import { onMounted } from 'vue'
@@ -13,9 +13,9 @@ onMounted (() => {
 <template>
   <div v-if="selectedServer?.hasPermission('profiler_edit')">
     <div class="flex pb-5 text-sm gap-x-1 items-center" v-if="selectedServer.ProfileState.IsEnabled && !selectedServer.ProfileState.HasCrashed">
-      <button v-if="!selectedServer.ProfileState.IsProfiling" class="r-send-button !text-green-400 !bg-green-800/20 hover:!bg-green-800/80 hover:!text-green-200" @click="toggleProfile(false)"><Play :size=18 /> Start Profile</button>
-      <button v-if="selectedServer.ProfileState.IsProfiling" class="r-send-button !text-slate-400 !bg-slate-500/20 hover:!bg-slate-500/80 hover:!text-slate-200" @click="toggleProfile(false)"><Square :size=18 /> End Profile</button>
-      <button v-if="selectedServer.ProfileState.IsProfiling" class="r-send-button !text-red-400 !bg-red-800/20 hover:!bg-red-800/80 hover:!text-red-200" @click="toggleProfile(true)"><Square :size=18 /> Cancel</button>
+      <button v-if="!selectedServer.ProfileState.IsProfiling" class="r-send-button !p-3 !text-green-400 !bg-green-800/20 hover:!bg-green-800/80 hover:!text-green-200" @click="toggleProfile(false)"><Play :size=18 /></button>
+      <button v-if="selectedServer.ProfileState.IsProfiling" class="r-send-button !p-3 !text-slate-400 !bg-slate-500/20 hover:!bg-slate-500/80 hover:!text-slate-200" @click="toggleProfile(false)"><Square :size=18 /></button>
+      <button v-if="selectedServer.ProfileState.IsProfiling" class="r-send-button !p-3 !text-red-400 !bg-red-800/20 hover:!bg-red-800/80 hover:!text-red-200 uppercase" @click="toggleProfile(true)"><X :size=18 /></button>
       <span class="flex ml-1 gap-x-1 bg-black/10 p-2">
         <div class="r-send-button !text-blue-400/50 !bg-red-800/0">FLAGS:</div>
         <button :class="[ 'r-send-button !animate-none !transition-none hover:bg-blue-800/20', selectedServer.ProfileFlags.CallMemory ? '!bg-blue-800/50 !text-blue-400' : '!bg-blue-800/10 !text-blue-400/50' ]" @click="selectedServer.ProfileFlags.CallMemory = !selectedServer.ProfileFlags.CallMemory; save()">Call Memory</button>
