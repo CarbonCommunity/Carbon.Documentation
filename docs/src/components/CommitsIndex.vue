53af8ca62f18f47cd3f16cf36cd9fe912041ec92
raul <raulssorban@gmail.com>
2025-12-21 18:55:44 +0100

    Polished and implemented new database

--- docs/src/components/CommitsIndex.vue
+++ docs/src/components/CommitsIndex.vue
@@ -1,77 +1,81 @@
 <script setup lang="ts">
-import { shallowRef, onMounted } from 'vue';
+import { shallowRef, onMounted, computed } from 'vue';
 import { Commit } from './commits/Commits';
 import { Search } from 'lucide-vue-next'
+import { BinaryReader } from '@/utils/BinaryReader'
 
 const searchInput = shallowRef<string>('')
-const searchResults = shallowRef<Commit[]>([])
+const searchResults = computed(() => {
+  const input = searchInput.value.toLowerCase()
+  return searchData.value.filter(x => x.Message.toLowerCase().includes(input) || x.Repository.toLowerCase().includes(input) || x.AuthorName.toLowerCase().includes(input))
+})
+const searchData = shallowRef<Commit[]>([])
 
 async function onSearch() {
-  searchResults.value.length = 0
-  
-  const response = await fetch('https://api.github.com/repos/CarbonCommunity/Carbon.Common/commits')
+  searchInput.value = ''
+}
+
+onMounted(async () => {
+  searchData.value.length = 0
+
+  const response = await fetch('https://api.carbonmod.gg/meta/git.dat')
 
   if (!response.ok) {
     throw new Error(`HTTP ${response.status}`)
   }
 
-  const results = await response.json()
+  const reader = new BinaryReader((await response.bytes()).buffer)
+  const count = reader.int32();
 
-  results.forEach((jsonCommit: any) => {
-    const commit = new Commit().parse(jsonCommit)
-    searchResults.value.push(commit)
-    console.log(commit)
-  });
+  for (let i = 0; i < count; i++) {
+    searchData.value.push(new Commit().parse(reader))    
+  }
   
-  searchInput.value = ''
-}
-
-onMounted(async () => {
-
+  searchData.value.sort((a, b) => b.Date?.getTime() - a.Date?.getTime());
 })
 </script>
 
 <template>
 <div class="mt-10">
   <div class="flex items-center text-slate-400 gap-x-2 bg-black/30 p-3 content-fill">
-    <Search :size="16" /> <input v-model="searchInput" @keyup.enter='onSearch' enter placeholder="Search commits..." class="w-full"/>
+    <Search :size="16" /> <input v-model="searchInput" @keyup.enter='onSearch' enter placeholder="Search commits (by message, repo name, author)..." class="w-full"/>
   </div>
 
   <div class="min-h-screen text-zinc-100">
     <div class="mx-auto w-full max-w-5xl px-6 py-10">
       <div class="space-y-10">
-        <article v-for="(item, i) in searchResults" :key="i" class="grid grid-cols-[240px_1fr] gap-10">
+        <article v-for="(commit, i) in searchResults.splice(0, 500)" :key="i" class="grid grid-cols-[240px_1fr] gap-10">
           <div class="flex items-center justify-end gap-4">
             <div class="text-right">
-              <a :href="item.AuthorUrl" class="text-sm font-semibold text-emerald-400 !no-underline">
-                {{ item.AuthorName }}
+              <a :href="commit.AuthorUrl" target="_blank" class="text-sm font-semibold text-emerald-400 !no-underline">
+                {{ commit.AuthorName }}
               </a>
               <div class="text-xs text-zinc-500">
-                <span v-if="item.wasToday()">
+                <span v-if="commit.wasToday()">
                   Today
                 </span>
                 <span v-else>
-                  {{ item.Date?.toDateString() }} 
-                  {{ item.Date?.toLocaleTimeString() }}
+                  {{ commit.Date?.toDateString() }} 
+                  {{ commit.Date?.toLocaleTimeString() }}
                 </span>
               </div>
             </div>
 
             <div class="relative">
-              <img v-if="item.AuthorAvatar" :src="item.AuthorAvatar" alt="" class="h-12 w-12 rounded-md object-cover ring-1 ring-white/10" />
+              <img v-if="commit.AuthorAvatar" :src="commit.AuthorAvatar" alt="" class="h-12 w-12 rounded-md object-cover ring-1 ring-white/10" />
               <div v-else class="h-12 w-12 rounded-md bg-zinc-800 ring-1 ring-white/10 grid place-items-center text-zinc-500">
               </div>
             </div>
           </div>
 
           <div class="pt-1">
-            <a :href="item.Url" class="inline-flex items-center gap-2 font-semibold text-emerald-400 hover:text-emerald-300">
-              <span class="truncate">{{ item.title }}</span>
-              <span class="text-amber-400">#{{ item.number }}</span>
-            </a>
+            <span class="inline-flex items-center font-semibold text-emerald-400 hover:text-emerald-300">
+              <a :href="commit.RepositoryUrl" target="_blank" class="truncate !no-underline">{{ commit.Repository }}</a>
+              <a :href="commit.CommitUrl" target="_blank" class="!text-amber-400 !no-underline">#{{ commit.Changeset }}</a>
+            </span>
 
             <div class="mt-2 text-sm text-zinc-200/90">
-                <span class="leading-6" v-html="item.Message"></span>
+                <span class="leading-6" v-html="commit.Message"></span>
             </div>
           </div>
         </article>
--- docs/src/components/commits/Commits.ts
+++ docs/src/components/commits/Commits.ts
@@ -1,10 +1,16 @@
+import { BinaryReader } from '@/utils/BinaryReader'
+
 export class Commit {
+  SHA: string = ''
+  Repository: string = ''
+  RepositoryUrl: string = ''
   AuthorName: string = ''
   AuthorAvatar: string = ''
   AuthorUrl: string = ''
   Message: string = ''
   Branch: string = ''
-  Url: string = ''
+  CommitUrl: string = ''
+  Changeset: number = 0
   Date: Date | null = null
 
   wasToday() {
@@ -14,13 +20,52 @@ export class Commit {
     return this.Date.getDate() == new Date(Date.now()).getDate()
   }
 
-  async parse(json: any) : Commit {
-    this.AuthorName = json.author.login
-    this.AuthorAvatar = json.author.avatar_url
-    this.AuthorUrl = json.author.html_url
-    this.Message = json.commit.message
-    this.Url = json.html_url
-    this.Date = new Date(json.commit.committer.date)
-    // this.Branch = (await (await fetch(json.url + "/branches-where-head")).json())[0].name
+  parse(reader: BinaryReader) : Commit {
+    this.Repository = reader.bstring()
+    this.SHA = reader.bstring()
+    this.Date = new Date(reader.bstring())
+    this.AuthorName = reader.bstring()
+    this.Message = reader.bstring()
+    this.Changeset = reader.int32()
+    this.RepositoryUrl = `https://github.com/CarbonCommunity/${this.Repository}`
+    this.CommitUrl = `${this.RepositoryUrl}/commit/${this.SHA}`
+
+    switch(this.AuthorName) {
+      case 'evs.ptr':
+        this.AuthorName = 'evs-ptr'
+        break;
+      case 'João Brázio':
+      case 'Joao Brazio':
+        this.AuthorName = 'jbrazio'
+        break;
+    }
+
+    let githubName = ''
+    switch(this.AuthorName) {
+      case 'raul':
+      case 'Raul-Sorin Sorban':
+        githubName = "raulssorban"
+        break;
+      case 'David':
+        githubName = 'david7-dev'
+        break;
+      case 'Death':
+        githubName = 'Deathicated'
+        break;
+      case 'Kopter':
+        githubName = 'kop7er'
+        break;
+      case 'evs-ptr':
+      case 'DezLife':
+      case 'jbrazio':
+      case 'ThePitereq':
+      case 'kop7er':
+      case 'Patrette':
+        githubName = this.AuthorName
+        break;
+    }
+    this.AuthorUrl = `https://github.com/${githubName}`
+    this.AuthorAvatar = `${this.AuthorUrl}.png`
+    return this
   }
 }
