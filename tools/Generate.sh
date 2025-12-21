d69430cb3eaa04399ea97721ef1c04dc81f2b9a1
evs.ptr <evs.ptr@proton.me>
2025-12-22 00:37:51 +0500

    `generator.yml` fixes + grouping + misc

--- tools/Generate.sh
+++ tools/Generate.sh
@@ -25,12 +25,15 @@ echo "Branch: $BRANCH"
 mkdir -p "$SERVER"
 mkdir -p "$DEPOT"
 
-echo "Downloading Carbon from the '$TAG' tag for $BUILD build..."
-curl -L -o "$ROOT/carbon.zip" "$CARBON_URL"
+echo "Downloading Carbon from the '$TAG' tag for $BUILD build, via $CARBON_URL ..."
+
+echo "::group::Carbon Setup"
+
+curl -L -o "$ROOT/carbon.tar.gz" "$CARBON_URL"
 
 echo "Extracting Carbon..."
-unzip -o "$ROOT/carbon.zip" -d "$SERVER"
-rm "$ROOT/carbon.zip"
+tar -xvf "$ROOT/carbon.tar.gz" -C "$SERVER"
+rm "$ROOT/carbon.tar.gz"
 
 if [ ! -f "$DEPOT/DepotDownloader" ]; then
     echo "Downloading DepotDownloader..."
@@ -46,12 +49,20 @@ if [ ! -f "$DEPOT/DepotDownloader" ]; then
     chmod +x "$DEPOT/DepotDownloader"
 fi
 
+echo "::endgroup::"
+
 echo "Downloading Rust server on $BRANCH branch via DepotDownloader..."
-cd "$DEPOT"
+cd "$DEPOT" || exit
 
-./DepotDownloader -app 258550 -branch "$BRANCH" -max-downloads 14 -dir "$SERVER"
+echo "::group::DepotDownloader Download"
 
-cd "$SERVER"
+echo 'regex:^(?!.*/StreamingAssets/)(?!.*items/.*\.json$).*$' > filelist.txt
+
+./DepotDownloader -app 258550 -branch "$BRANCH" -max-downloads 16 -filelist "filelist.txt" -dir "$SERVER"
+
+echo "::endgroup::"
+
+cd "$SERVER" || exit
 echo "Starting server..."
 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${SERVER}/RustDedicated_Data/Plugins/x86_64"
@@ -61,13 +72,30 @@ source "carbon/tools/environment.sh"
 
 chmod +x RustDedicated
 
+set +e
+
+echo "::group::Server Run"
+
 ./RustDedicated -batchmode -nographics -logs -silent-crashes \
     -server.hostname "Generator" -server.identity "generator" \
-    -server.saveinterval 400 -server.maxplayers 1 -chat.serverlog 1 \
+    -server.maxplayers 1 -chat.serverlog 1 \
     -app.port 1- \
     -aimanager.nav_disable 1 \
     -disable-server-occlusion -disable-server-occlusion-rocks -disableconsolelog -skipload -noconsole \
     +server.level "CraggyIsland" -insecure \
     -logfile 2>&1
+
+echo "::endgroup::"
+
+EXIT_CODE=$?
+
+set -e
+
+if [ $EXIT_CODE -eq 0 ] || [ $EXIT_CODE -eq 137 ]; then
+    echo "Server finished (Exit code: $EXIT_CODE). Treating as success."
+else
+    echo "Server failed with unexpected exit code: $EXIT_CODE"
+    exit $EXIT_CODE
+fi
 
 exit 0
